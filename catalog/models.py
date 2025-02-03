91574756e93b57193ed1e5a8838dccdef98f1e7a
Luke-N-Benson <113860958+Luke-N-Benson@users.noreply.github.com>
2025-02-03 11:07:50 -0500

    Add model migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 
 from django.db.models import UniqueConstraint # Constrains fields to unique values
