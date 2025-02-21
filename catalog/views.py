964939994736ac2d66eff0064a4eaa697dbffe24
Luke-N-Benson <113860958+Luke-N-Benson@users.noreply.github.com>
2025-02-21 14:06:36 -0500

    Adds book and author lists

--- catalog/views.py
+++ catalog/views.py
@@ -31,3 +31,22 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='the')[:5] # Get 5 books containing the title war
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'author_list'   # your own name for the list as a template variable
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
